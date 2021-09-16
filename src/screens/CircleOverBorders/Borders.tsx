3e4669e9d01682ca456f320264ab2df90cb2c8c3
Sergey Zakharov <zakharov.sergey1996@gmail.com>
2021-09-17 02:36:31 +0300

    Removed unused components

--- src/screens/CircleOverBorders/Borders.tsx
+++ src/screens/CircleOverBorders/Borders.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {useTheme} from '../../assets/hooks';
 import {BORDERS} from '../../assets/constants';
 
