346937411b30d416cc1a5db18542b2fc91e19829
Sergey Zakharov <noxcontest@Sergeys-MacBook-Pro.local>
2022-02-01 00:42:25 +0200

    dragging cubes

--- src/screens/DragAndDrop/Components/Cube.tsx
+++ src/screens/DragAndDrop/Components/Cube.tsx
@@ -1,5 +1,5 @@
-import {View, Text, StyleSheet} from 'react-native';
-import React from 'react';
+import {Text, StyleSheet, PanResponder, Animated} from 'react-native';
+import React, {useRef} from 'react';
 import {defaultBorderRadius} from '../../../utils/constants';
 import {useTheme} from '../../../utils/hooks';
 
@@ -8,14 +8,39 @@ interface IProps {
 }
 
 const SIZE = 64;
+const INITIAL_POINTS = {x: 0, y: 0};
 
 export const Cube = (props: IProps) => {
   const {styles} = useStyles();
 
+  // drag'n'drop refs
+  const translation = useRef(new Animated.ValueXY(INITIAL_POINTS)).current;
+  const panResponderHandler = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, gestures) => {
+        translation.setValue({x: gestures.dx, y: gestures.dy});
+      },
+      onPanResponderRelease: () => {
+        Animated.spring(translation, {
+          toValue: INITIAL_POINTS,
+          useNativeDriver: true,
+        }).start();
+      },
+    }),
+  ).current;
+
   return (
-    <View style={styles.container}>
+    <Animated.View
+      style={[
+        styles.container,
+        {
+          transform: [{translateX: translation.x}, {translateY: translation.y}],
+        },
+      ]}
+      {...panResponderHandler.panHandlers}>
       <Text style={styles.text}>{props.title ?? 'Title'}</Text>
-    </View>
+    </Animated.View>
   );
 };
 
